07e24098d0ae793109150ed66ddcb18acc15c97f
Mohamad-Tayara <280333031+Mohamad-Tayara@users.noreply.github.com>
2026-05-12 12:36:27 +0200

    neuer code
    bla bla

---
+++ versuche.py
@@ -0,0 +1,22 @@
+'''def total_price(price_p_item, num_items, tax = 0.19):  #tax ist ein default Parameter, d.h. wenn ich für diesen Paramter nicht explizit ein Argument übergebe, wird hier defaultmäßig 0.0 verwendet
+    total_price = price_p_item * num_items
+    total_price  += total_price * tax
+    return total_price
+
+total_price(4.99, 3)
+print(total_price(4.99, 3))
+'''
+#######################
+'''def person(name, alter, adresse, beruf):
+    print(f"{name} ist {alter} Jahre alt. {name} arbeitet als {beruf} in {adresse}.")
+    person("Gordon", 32, "Black Mesa", "Laborant")
+    person("Alyxaandra",23, "Berlin", "Künstlerin")'''
+########################
+'''def summen(*args):
+    result = sum(args)
+    return result
+print(summen(1, 2, 3))  # 6
+print(summen(4, 5))     # 9'''
+#########################
+print?
+x =Y
